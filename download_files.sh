ab6d993347cb66c814b5a670fe4cc8c0908b556a
Mayankraj6 <Mayankraj.a6c@gmail.com>
2025-02-21 23:44:57 +0530

    Create download_files.sh

---
+++ download_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Downloading files from Google Drive..."
+
+# Google Drive folder link
+GDRIVE_LINK="https://drive.google.com/drive/folders/1CKMO5INyndp5l0PrZh64YiJ9aU6h_RPN?usp=sharing"
+
+echo "Please visit the following link to manually download the files:"
+echo "$GDRIVE_LINK"
+
+echo "Download complete!"
